f88cae294dfee1800ab5fb27af2671963c7307dc
Matt <matt@leenox.(none)>
2016-04-04 02:41:33 -0700

    csv progress

---
+++ features/exists.feature
@@ -0,0 +1,5 @@
+Scenario: CSV submit form exists
+Given I am on the main page
+Given at least one company is visible
+Given I press on the 1 month tab
+Then the x axis will be numbered 0 to 22
---
+++ features/step_definitions/home.rb
@@ -0,0 +1,15 @@
+Given(/^I am on the main page$/) do
+   browser.get('http://localhost')
+end
+
+Then(/^there is a graph$/) do
+   browser.find_element(:id, 'LineChart').displayed?   
+end
+
+Then(/^there is a CSV upload form$/) do
+   browser.find_element(:id, 'transaction').displayed?
+end
+
+Then(/^there is an account list$/) do
+   browser.find_element(:id, 'portfolio').displayed?
+end
